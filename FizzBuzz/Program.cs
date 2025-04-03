ccdad0ea6631c33222b5ae813de1f222030cd0e4
omar mohamed <omarmohamed2342@outlook.com>
2025-04-03 19:09:32 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        string input = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb It's fleece was white as snow";
+        var result = FizzBuzzDetector.GetOverlappings(input);
+        Console.WriteLine("Output String: " + result.OutputString);
+        Console.WriteLine("Fizz Count: " + result.FizzCount);
+        Console.WriteLine("Buzz Count: " + result.BuzzCount);
+        Console.WriteLine("FizzBuzz Count: " + result.FizzBuzzCount);
+    }
+}
+public class FizzBuzzDetector
+{
+    public static FizzBuzzOutput GetOverlappings(string input)
+    {
+        if (input == null) throw new ArgumentNullException(nameof(input));
+
+        int FizzCount = 0;
+        int BuzzCount = 0;
+        int FizzBuzzCount = 0;
+        string outputString = string.Empty;
+
+        string[] words = input.Split(' ');
+        int alphanumericCount = 0;
+        foreach (string word in words)
+        {
+            if (IsAlphanumeric(word))
+            {
+                string replacmentWord = word;
+                alphanumericCount++;
+
+                if (alphanumericCount % 15 == 0)
+                {
+                    FizzBuzzCount++;
+                    replacmentWord = "FizzBuzz";
+                }
+                else if (alphanumericCount % 3 == 0)
+                {
+                    FizzCount++;
+                    replacmentWord = "Fizz";
+                }
+                else if (alphanumericCount % 5 == 0)
+                {
+                    BuzzCount++;
+                    replacmentWord = "Buzz";
+                }
+
+                outputString += replacmentWord + ' ';
+            }
+            else
+            {
+                outputString += word;
+            }
+        }
+
+        return new FizzBuzzOutput(outputString, FizzCount, BuzzCount, FizzBuzzCount);
+    }
+    public static bool IsAlphanumeric(string word)
+    {
+        return Regex.IsMatch(word, @"\w+");
+    }
+}
+
+public record FizzBuzzOutput(string OutputString, int FizzCount, int BuzzCount, int FizzBuzzCount);
---
+++ FuzzBuzzTests/FizzBuzzDetectorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace FizzBuzzTests
+{
+    [TestFixture]
+    public class FizzBuzzDetectorTests
+    {
+        [Test]
+        public void GetOverlappings_WhenInputIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => FizzBuzzDetector.GetOverlappings(null));
+        }
+        [Test]
+        public void IsAlphanumeric_WhenWordIsAlphanumeric_ReturnsTrue()
+        {
+            ClassicAssert.IsTrue(FizzBuzzDetector.IsAlphanumeric("word"));
+        }
+        [Test]
+        public void IsAlphanumeric_WhenWordIsNotAlphanumeric_ReturnsFalse()
+        {
+            Assert.That(FizzBuzzDetector.IsAlphanumeric("!@#$%^"), Is.False);
+        }
+
+        [Test]
+        public void GetOverlappings_WhenInputContainsMultiplesOf3_FizzCountIsCorrect()
+        {
+            string input = "one two three four five six seven eight nine";
+
+            var result = FizzBuzzDetector.GetOverlappings(input);
+
+            Assert.That(result.FizzCount, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GetOverlappings_WhenInputContainsMultiplesOf5_BuzzCountIsCorrect()
+        {
+            string input = "one two three four five six seven eight nine ten";
+
+            var result = FizzBuzzDetector.GetOverlappings(input);
+
+            Assert.That(result.BuzzCount, Is.EqualTo(2));
+        }
+        [Test]
+        public void GetOverlappings_WhenInputContainsMultiplesOf15_FizzBuzzCountIsCorrect()
+        {
+            string input = "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty-one twenty-two twenty-three twenty-four twenty-five twenty-five twenty-five twenty-five twenty-five thirty";
+
+            var result = FizzBuzzDetector.GetOverlappings(input);
+
+            Assert.That(result.FizzBuzzCount, Is.EqualTo(2));
+        }
+    }
+}
